746b266a3733dfbd71b8ea5e9d458667f9c61259
unknown <grawling08@gmail.com>
2017-06-03 14:39:03 +0800

    summary(rush)

--- Payroll2/Reports/frmReports.vb
+++ Payroll2/Reports/frmReports.vb
@@ -3,7 +3,7 @@
     Private Sub frmReports_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         GetPayrollBatch(current_cutoff, current_company)
         Dim objRpt As New CrystalReport1
-        objRpt.SetDataSource(ds.Tables(1))
+        objRpt.SetDataSource(ds.Tables(0))
         CrystalReportViewer1.ReportSource = objRpt
         CrystalReportViewer1.Refresh()
     End Sub
